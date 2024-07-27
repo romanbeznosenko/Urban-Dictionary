85a236aa9324f852f671835d51dfea42162d6863
Roman Beznosenko <245125@edu.p.lodz.pl>
2024-07-27 20:46:42 +0200

    Updated xpath to divs

--- functions.py
+++ functions.py
@@ -6,42 +6,37 @@ from lxml import html
 def answer(text):
 	words = text.split(" ")
 	new_request = ""
-	for i in words:
-		new_request += i + "%20"
-	link = 'https://www.urbandictionary.com/define.php?term=' + new_request
+	for i in range(len(words)):
+		new_request += words[i] + "%20"
+	# for i in words:
+	# 	new_request += i + "%20"
+	print(new_request[:-3])
+	link = 'https://www.urbandictionary.com/define.php?term=' + new_request[:-3]
 	agent = {"User-Agent":"Mozilla/5.0"}
 	resp = req.get(link,headers = agent)
 	tree = html.fromstring(resp.content)
-	header = tree.xpath("""/html/body[@class='font-sans text-lg bg-revell dark:bg-chocolate pb-48']
-		/div[@id='ud-root']/div[@class='md:pt-0 pt-[8.5rem]']
-		/main[@class='px-3 md:px-0']
-		/div[@class='container mx-auto py-4 max-w-[970px]']
-		/div[@class='flex flex-col lg:flex-row mx-0 gap-4']
-		/section[@class='flex-1']
-		/div[@class='definition bg-white mb-4 shadow-light dark:bg-yankees dark:text-white rounded-md overflow-hidden'][1]/div[@class='p-5 md:p-8']
-		/div[@class='mb-8 flex']/h1[@class='flex-1']""")
+	header = tree.xpath("""/html/body[@class='font-sans text-lg bg-revell dark:bg-chocolate pb-48']/div[@id='ud-root']
+					 /div[@class='md:pt-0 pt-[3.5rem]']/main[@class='px-3 md:px-0']
+					 /div[@class='container mx-auto py-4 max-w-[970px]']
+					 /div[@class='flex flex-col lg:flex-row mx-0 gap-4']/section[@class='flex-1']
+					 /div[@class='definition bg-white mb-4 shadow-light dark:bg-yankees dark:text-white rounded-md overflow-hidden'][1]
+					 /div[@class='p-5 md:p-8']/div[@class='mb-8 flex']/h1[@class='flex-1']""")
 
 	header = "".join(["*" + i.text_content() + "*" for i in header]) + "\n"
 
 	defenition = tree.xpath("""/html/body[@class='font-sans text-lg bg-revell dark:bg-chocolate pb-48']
-		/div[@id='ud-root']/div[@class='md:pt-0 pt-[8.5rem]']/main[@class='px-3 md:px-0']
-		/div[@class='container mx-auto py-4 max-w-[970px]']
-		/div[@class='flex flex-col lg:flex-row mx-0 gap-4']
-		/section[@class='flex-1']
-		/div[@class='definition bg-white mb-4 shadow-light dark:bg-yankees dark:text-white rounded-md overflow-hidden']
-		/div[@class='p-5 md:p-8']/div[@class='break-words meaning mb-4']""")
+						 /div[@id='ud-root']/div[@class='md:pt-0 pt-[3.5rem]']/main[@class='px-3 md:px-0']
+						 /div[@class='container mx-auto py-4 max-w-[970px]']/div[@class='flex flex-col lg:flex-row mx-0 gap-4']
+						 /section[@class='flex-1']/div[@class='definition bg-white mb-4 shadow-light dark:bg-yankees dark:text-white rounded-md overflow-hidden'][1]
+						 /div[@class='p-5 md:p-8']/div[@class='break-words meaning mb-4']""")
 
 	defenition = "\n-------------------------------\n".join(i.text_content() for i in defenition) + "\n"
 
-	example = tree.xpath("""/html/body[@class='font-sans text-lg bg-revell dark:bg-chocolate pb-48']
-		/div[@id='ud-root']/div[@class='md:pt-0 pt-[8.5rem]']
-		/main[@class='px-3 md:px-0']
-		/div[@class='container mx-auto py-4 max-w-[970px]']
-		/div[@class='flex flex-col lg:flex-row mx-0 gap-4']
-		/section[@class='flex-1']
-		/div[@class='definition bg-white mb-4 shadow-light dark:bg-yankees dark:text-white rounded-md overflow-hidden'][1]
-		/div[@class='p-5 md:p-8']
-		/div[@class='break-words example italic mb-4']""")
+	example = tree.xpath("""/html/body[@class='font-sans text-lg bg-revell dark:bg-chocolate pb-48']/div[@id='ud-root']
+					  /div[@class='md:pt-0 pt-[3.5rem]']/main[@class='px-3 md:px-0']/div[@class='container mx-auto py-4 max-w-[970px]']
+					  /div[@class='flex flex-col lg:flex-row mx-0 gap-4']/section[@class='flex-1']
+					  /div[@class='definition bg-white mb-4 shadow-light dark:bg-yankees dark:text-white rounded-md overflow-hidden'][1]
+					  /div[@class='p-5 md:p-8']/div[@class='break-words example italic mb-4']""")
 
 	example = "\n".join(["_" + i.text_content() + "_" for i in example])
 
